b146c4f63abe4bf40e90fbce950078eb9db87a5b
perunhimself <perunworks@users.noreply.github.com>
2017-03-29 14:18:08 +0800

    Added Oinkmaster install

--- zenected_install.sh
+++ zenected_install.sh
@@ -62,7 +62,7 @@ echo -e "4. Installing and configuring IPS"
 
 ##as described here: https://redmine.openinfosecfoundation.org/projects/suricata/wiki/Ubuntu_Installation
 
-sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev \
+sudo apt-get -y install oinkmaster libpcre3 libpcre3-dbg libpcre3-dev \
 build-essential autoconf automake libtool libpcap-dev libnet1-dev \
 libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 \
 make libmagic-dev libjansson-dev libjansson4 pkg-config
